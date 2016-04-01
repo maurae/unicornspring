ecee3905f93b85176d805bff713c52d02ccfb643
vagrant <vagrant@precise32.(none)>
2016-04-01 23:16:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unicornspring::Application.config.secret_key_base = 'a2ac730f06cccfc07e1b47c5a72aafff110e67a8d79710f833f1a831ff2382448bf0a9511d24a7087ec3569bf9d5fa61a2e7fffa3735a513e11feb55e9da8bab'
